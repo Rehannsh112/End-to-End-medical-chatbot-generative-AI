2558e4446b282ad660af2cc303e2dc8a4a39a870
Rehan Shaikh <rehannsh112@gmail.com>
2026-02-02 19:23:32 +1030

    folder structure added.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+name = 'Generative AI Project',
+version= '0.0.0',
+author= 'Rehan Shaikh',
+author_email= 'rehannsh112@gmail.com',
+packages= find_packages(),
+install_requires = []
+
+)
---
+++ template.py
@@ -0,0 +1,32 @@
+import os
+from pathlib import Path
+import logging
+
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
+
+list_of_files = [
+    "src/__init__.py",
+    "src/helper.py",
+    "src/prompt.py",
+    ".env",
+    "setup.py",
+    "app.py",
+    "test.py",
+    "research/trials.ipynb",
+]
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir = filepath.parent
+
+    # create directory if needed
+    if str(filedir) != ".":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Creating directory; {filedir} for the file: {filepath.name}")
+
+    # create empty file if missing or empty
+    if (not filepath.exists()) or (filepath.stat().st_size == 0):
+        filepath.touch()
+        logging.info(f"Creating empty file: {filepath}")
+    else:
+        logging.info(f"{filepath.name} already exists")
